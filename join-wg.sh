1b4dff01c5f65521376a82c887a3d041034845e8
Grey Christoforo <grey@christoforo.net>
2020-09-17 16:47:16 +0100

    Update join-wg.sh

--- join-wg.sh
+++ join-wg.sh
@@ -25,7 +25,9 @@ chmod +x /bin/wg-request >/dev/null 2>/dev/null
 
 wg genkey | tee /tmp/peer_A.key | wg pubkey > /tmp/peer_A.pub
 timeout 5 python3 /bin/wg-request --private-key $(cat /tmp/peer_A.key) $(cat /tmp/peer_A.pub) "${PEER}" > "/etc/wireguard/${IFACE}.conf" 2>/dev/null
-if test "$?" -ne "0"
+RC=$?
+echo "rc = ${RC}"
+if test "${RC}" -ne "0"
 then
   echo "New config written to /etc/wireguard/${IFACE}.conf"
   cat "/etc/wireguard/${IFACE}.conf"
